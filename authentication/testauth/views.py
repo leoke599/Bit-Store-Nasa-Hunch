a25810dcb6c8089ff259ec383f8f56a10ecb6b39
oOXxTNTxXOo <bob.bross10@gmail.com>
2024-10-10 10:58:00 -0600

    homepage request response

--- authentication/testauth/views.py
+++ authentication/testauth/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def homepage(request):
+    
+    return HttpResponse("this is the homepage")
+
+def register(request):
+
+    pass
+
+def my_login(request):
+
+    pass
+
+def dashboard(request):
+
+    pass
