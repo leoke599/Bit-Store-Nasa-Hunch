5db522c644370e2f84f2a56d144e8f39cb3a583e
oOXxTNTxXOo <bob.bross10@gmail.com>
2024-10-23 00:15:42 -0600

    basic sign in

--- authentication/testauth/forms.py
+++ authentication/testauth/forms.py
@@ -1,6 +1,13 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from django.contrib.auth.models import User
+
+from django import forms
+
+from django.forms.widget import PasswordInput, TextInput
+
+
+# - creates/registers a user (model form)
 
 class CreateUserForm(UserCreationForm):
 
@@ -8,3 +15,10 @@ class CreateUserForm(UserCreationForm):
 
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+# - logs a user in (model form)
+
+class LoginForm(AuthenticationForm):
+
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
